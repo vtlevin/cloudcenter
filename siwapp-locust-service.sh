68ae7b5e1c8a223f15f69c57356a6661369a56ce
vtlevin <troylevin@gmail.com>
2019-04-06 13:40:13 -0400

    Create siwapp-locust-service.sh

---
+++ siwapp-locust-service.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo  "Waiting for website to be reachable..."
+COUNT=0
+MAX=100
+SLEEP_TIME=10
+ERR=0
+
+LOCUST_COUNT = 10
+
+# Keep checking for haproxy to give proper 401 return to login
+until [ "$(curl --write-out %{http_code} --silent --output /dev/null ${CliqrTier_siwapp_haproxy_app_IP})" -eq "401" ]; do
+    sleep ${SLEEP_TIME}
+    let "COUNT++"
+    echo ${COUNT}
+    if [ ${COUNT} -gt ${MAX} ]; then
+        ERR=1
+        break
+    fi
+done
+if [ ${ERR} -ne 0 ]; then
+    echo "Failed to get proper response from haproxy, so guessing something is wrong."
+    exit 1
+fi
+
+nohup /home/usr/venv/bin/python /usr/share/venv/bin/locust --locustfile=/usr/share/systemd/siwapp-locust-file.py --host=http://${CliqrTier_siwapp_haproxy_app_IP} &>/dev/null &
+sleep 5
+nohup curl -X POST -F "locust_count=${LOCUST_COUNT}" -F "hatch_rate=10" http://localhost:8089/swarm &>/dev/null &
+while :
+do
+    true
+done
