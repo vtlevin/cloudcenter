c48acd70295426143b64f0a9589373e80cc21abf
vtlevin <troylevin@gmail.com>
2019-04-07 06:55:28 -0400

    Update load-simulator.sh

--- load-simulator.sh
+++ load-simulator.sh
@@ -14,6 +14,21 @@ sudo yum install -y python-pip
 sudo pip install pip --upgrade
 sudo pip install virtualenv
 
+# Hack to address the limitation of public ip's nopt being configuired for 5.0 release
+cloudType=`cat /usr/local/osmosix/etc/cloud`
+if [ $cloudType == 'vmware' ]
+then
+#    wget https://github.com/vtlevin/cloudcenter/edit/master/publicips.txt
+#    chmod +x publicips.txt
+#    . publicips.txt
+    CliqrTier_siwapp_haproxy_db_PUBLIC_IP=$CliqrTier_siwapp_haproxy_db_IP
+    CliqrTier_siwapp_mariadb_PUBLIC_IP=$CliqrTier_siwapp_mariadb_IP
+    CliqrTier_siwapp_app_PUBLIC_IP=$CliqrTier_siwapp_app_IP
+    CliqrTier_siwapp_load_simulator_PUBLIC_IP=$CliqrTier_siwapp_load_simulator_IP
+    CliqrTier_siwapp_haproxy_app_PUBLIC_IP=$CliqrTier_siwapp_haproxy_app_IP
+    cliqrNodePublicIp=$cliqrNodePrivateIp
+fi
+
 # Set internal separator to ',' since they're comma-delimited lists.
 temp_ifs=${IFS}
 IFS=','
@@ -25,17 +40,16 @@ for host in $CliqrTier_siwapp_haproxy_app_HOSTNAME ; do
     sudo su -c "echo '${ipArr[${host_index}]} ${host}' >> /etc/hosts"
     let host_index=${host_index}+1
 done
-# Set internal separator to ',' since they're comma-delimited lists.
-temp_ifs=${IFS}
-IFS=','
-ipArr=(${CliqrTier_siwapp_app_PUBLIC_IP}) # Array of IPs in my tier.
+# Set internal separator back to original.
+IFS=${temp_ifs}
 
-# Iterate through list of hosts to add hosts and corresponding IPs to haproxy config file.
 host_index=0
 for host in $CliqrTier_siwapp_app_HOSTNAME ; do
     sudo su -c "echo '${ipArr[${host_index}]} ${host}' >> /etc/hosts"
     let host_index=${host_index}+1
 done
+# Set internal separator back to original.
+IFS=${temp_ifs}
 
 # Install virtualenv and locustio
 virtualenv /usr/share/venv
