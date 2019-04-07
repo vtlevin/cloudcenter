54eb3943a9af25db2d6eaa0ec6ca913b6a150992
vtlevin <troylevin@gmail.com>
2019-04-07 07:51:46 -0400

    Update load-simulator.sh

--- load-simulator.sh
+++ load-simulator.sh
@@ -40,16 +40,18 @@ for host in $CliqrTier_siwapp_haproxy_app_HOSTNAME ; do
     sudo su -c "echo '${ipArr[${host_index}]} ${host}' >> /etc/hosts"
     let host_index=${host_index}+1
 done
-# Set internal separator back to original.
-IFS=${temp_ifs}
 
+# Set internal separator to ',' since they're comma-delimited lists.
+temp_ifs=${IFS}
+IFS=','
+ipArr=(${CliqrTier_siwapp_app_PUBLIC_IP}) # Array of IPs in my tier.
+
+# Iterate through list of hosts to add hosts and corresponding IPs to haproxy config file.
 host_index=0
 for host in $CliqrTier_siwapp_app_HOSTNAME ; do
     sudo su -c "echo '${ipArr[${host_index}]} ${host}' >> /etc/hosts"
     let host_index=${host_index}+1
 done
-# Set internal separator back to original.
-IFS=${temp_ifs}
 
 # Install virtualenv and locustio
 virtualenv /usr/share/venv
