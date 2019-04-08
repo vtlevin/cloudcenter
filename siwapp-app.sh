92966487a224119c281ebb99f84a82b13d23ee08
vtlevin <troylevin@gmail.com>
2019-04-08 09:07:17 -0400

    Update siwapp-app.sh

--- siwapp-app.sh
+++ siwapp-app.sh
@@ -46,11 +46,11 @@ sudo mv /etc/yum.repos.d/cliqr.repo ~
 agentSendLogMessage "Starting app install script"
 sudo yum -y update
 #sudo yum -y install git httpd php php-mysql php-xml php-mbstring
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-sudo yum install yum-utils
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum -y install yum-utils
 sudo yum-config-manager --enable remi-php56
-sudo yum install git httpd php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+sudo yum -y install git httpd php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
 sudo yum clean all
 
 sudo git clone https://github.com/siwapp/siwapp-sf1.git /var/www/html/
