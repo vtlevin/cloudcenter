838da7100559a67212bc333713c54ead8eae3ba6
vtlevin <troylevin@gmail.com>
2019-04-09 12:27:27 -0400

    Update appd-php-agent-sample.sh

--- appd-php-agent-sample.sh
+++ appd-php-agent-sample.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-APPD_PHP_PATH=/opt/remi/php56/root/bin
-APPD_CONF_ACCOUNT_NAME='dangerous2019031501233911'
-APPD_CONF_ACCESS_KEY='xhibem365na4'
-APPD_CONF_CONTROLLER_HOST='dangerous2019031501233911.saas.appdynamics.com'
-APPD_CONF_CONTROLLER_PORT='443'
-APPD_CONF_SSL_ENABLED='true'
-APPD_CONF_APP='SIWAPP'
-APPD_CONF_TIER='AppTier'
-AGENT_LOCATION="/opt/appd/php-agent"
+APPD_PHP_PATH='APPD_PHP_PATH=/opt/remi/php56/root/bin'
+APPD_CONF_ACCOUNT_NAME='APPD_CONF_ACCOUNT_NAME=dangerous2019031501233911'
+APPD_CONF_ACCESS_KEY='APPD_CONF_ACCESS_KEY=xhibem365na4'
+APPD_CONF_CONTROLLER_HOST='APPD_CONF_CONTROLLER_HOST=dangerous2019031501233911.saas.appdynamics.com'
+APPD_CONF_CONTROLLER_PORT='APPD_CONF_CONTROLLER_PORT=443'
+APPD_CONF_SSL_ENABLED='APPD_CONF_SSL_ENABLED=true'
+APPD_CONF_APP='APPD_CONF_APP=SIWAPP'
+APPD_CONF_TIER='APPD_CONF_TIER=AppTier'
 
 #sudo mkdir -p /opt/appdynamics/php-agent
 #sudo mkdir -p /opt/appdynamics/php-agent/logs
@@ -18,7 +17,11 @@ AGENT_LOCATION="/opt/appd/php-agent"
 #chmod +x runme.sh
 #chmod +x installVars
 
-APPD_PHP_PATH=/opt/remi/php56/root/bin rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
+echo "198.169.195.125 dangerous2019031501233911.saas.appdynamics.com" >>  /etc/hosts
+
+source installVars
+
+sudo $APPD_PHP_PATH $APPD_CONF_ACCOUNT_NAME $APPD_CONF_ACCESS_KEY $APPD_CONF_CONTROLLER_HOST $APPD_CONF_CONTROLLER_PORT $APPD_CONF_SSL_ENABLED $APPD_CONF_APP $APPD_CONF_TIER rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
 
 sudo systemctl stop httpd
 sudo systemctl start httpd
