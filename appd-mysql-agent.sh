9bc1e996540a69e10fc466612282f6fdf7d4d8bd
vtlevin <troylevin@gmail.com>
2019-04-09 18:02:56 -0400

    Update appd-mysql-agent.sh

--- appd-mysql-agent.sh
+++ appd-mysql-agent.sh
@@ -2,4 +2,4 @@
 
 echo "198.169.195.125 dangerous2019031501233911.saas.appdynamics.com" >> /etc/hosts
 
-sudo java -jar /opt/appd/mysql/db-agent.jar
+sudo java -jar /opt/appd/mysql-agent/db-agent.jar
