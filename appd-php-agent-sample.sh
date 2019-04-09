4b28124b7622ae604f297565698b1487d6ba9505
vtlevin <troylevin@gmail.com>
2019-04-09 11:50:50 -0400

    Update appd-php-agent-sample.sh

--- appd-php-agent-sample.sh
+++ appd-php-agent-sample.sh
@@ -8,8 +8,16 @@ APPD_CONF_CONTROLLER_PORT='443'
 APPD_CONF_SSL_ENABLED='true'
 APPD_CONF_APP='SIWAPP'
 APPD_CONF_TIER='AppTier'
-AGENT_LOCATION="/opt/appdynamics/php-agent"
+AGENT_LOCATION="/opt/appd/php-agent"
 
-APPD_PHP_PATH=/opt/remi/php56/root/bin sudo bash ./runme.sh
+#sudo mkdir -p /opt/appdynamics/php-agent
+#sudo mkdir -p /opt/appdynamics/php-agent/logs
 
-systemctl restart httpd
+#chown -R apache:apache /opt/appdynamics/php-agent
+#chmod -R 755 /opt/appdynamics/php-agent/logs
+#chmod +x runme.sh
+#chmod +x installVars
+
+APPD_PHP_PATH=/opt/remi/php56/root/bin rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
+
+sudo systemctl restart httpd
