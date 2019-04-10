84e5de8da5b6c94b4f91fa849871e00c7d55cef1
vtlevin <troylevin@gmail.com>
2019-04-10 06:11:03 -0400

    Update appd-machine-agent.sh

--- appd-machine-agent.sh
+++ appd-machine-agent.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
+
+agentSendLogMessage "Installing Appd Machine-Agent."
 
 /opt/appd/machine-agent/jre/bin/java -jar 
 /opt/aapd/machine-agent/machineagent.jar
+
+agentSendLogMessage "App install script complete"
