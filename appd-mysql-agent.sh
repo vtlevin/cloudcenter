9a5d92452287e9e1472363aa6b5823065e7f3961
vtlevin <troylevin@gmail.com>
2019-04-10 05:45:07 -0400

    Update appd-mysql-agent.sh

--- appd-mysql-agent.sh
+++ appd-mysql-agent.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "198.169.195.125 dangerous2019031501233911.saas.appdynamics.com" >> /etc/hosts
-
 sudo yum install java-1.8.0-openjdk-headless -y
 
 sudo java -jar /opt/appd/mysql-agent/db-agent.jar
