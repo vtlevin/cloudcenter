8a07487a6333b4db30e6a62eed59bd51a95980d8
vtlevin <troylevin@gmail.com>
2019-04-10 14:35:15 -0400

    Update siwapp-app.sh

--- siwapp-app.sh
+++ siwapp-app.sh
@@ -128,7 +128,6 @@ agentSendLogMessage "Install Tetration Agent"
 curl https://raw.githubusercontent.com/vtlevin/cloudcenter/master/instant-pov_installer_enforcer_linux.sh | sudo bash
 
 agentSendLogMessage "Install AppD PHP-Agent"
-chown -R apache:apache /opt/appd/php-agent
 curl https://raw.githubusercontent.com/vtlevin/cloudcenter/master/appd-php-agent.sh | sudo bash
 
 agentSendLogMessage "Install AppD MACHINE-Agent"
