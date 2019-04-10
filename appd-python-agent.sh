4bccd77741e0217e007985300244b44bfca8c773
vtlevin <troylevin@gmail.com>
2019-04-10 07:11:45 -0400

    Update appd-python-agent.sh

--- appd-python-agent.sh
+++ appd-python-agent.sh
@@ -2,4 +2,6 @@
 
 agentSendLogMessage "Installing Appd PYTHON-Agent."
 
+/usr/share/venv/bin/pip install appdynamics
+
 pyagent run -c <path_to_appdynamics_config_file> -- gunicorn -w 8 -b '0.0.0.0:9000' example.app:application
