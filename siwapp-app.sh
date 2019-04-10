0cb7d1ecd676f826ab6ba3503c662cd12f1a1ba9
vtlevin <troylevin@gmail.com>
2019-04-10 15:33:34 -0400

    Update siwapp-app.sh

--- siwapp-app.sh
+++ siwapp-app.sh
@@ -118,14 +118,14 @@ agentSendLogMessage "App install script complete"
 agentSendLogMessage "Installing Java."
 sudo yum install java-1.8.0-openjdk-headless -y
 
-agentSendLogMessage "Installing Tet Pre-reqs."
+#agentSendLogMessage "Installing Tet Pre-reqs."
 
-sudo yum -y install ipset
-sudo yum -y install unzip
+#sudo yum -y install ipset
+#sudo yum -y install unzip
 
-agentSendLogMessage "Install Tetration Agent"
+#agentSendLogMessage "Install Tetration Agent"
 # Get Tet Script
-curl https://raw.githubusercontent.com/vtlevin/cloudcenter/master/instant-pov_installer_enforcer_linux.sh | sudo bash
+#curl https://raw.githubusercontent.com/vtlevin/cloudcenter/master/instant-pov_installer_enforcer_linux.sh | sudo bash
 
 agentSendLogMessage "Install AppD PHP-Agent"
 curl https://raw.githubusercontent.com/vtlevin/cloudcenter/master/appd-php-agent.sh | sudo bash
