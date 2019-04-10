62b7c91e6e305340cf147728cb388c4e41a78569
vtlevin <troylevin@gmail.com>
2019-04-10 06:11:34 -0400

    Update appd-mysql-agent.sh

--- appd-mysql-agent.sh
+++ appd-mysql-agent.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+
+agentSendLogMessage "Installing Appd MYSQL-Agent."
 
 sudo java -jar /opt/appd/mysql-agent/db-agent.jar
+
+agentSendLogMessage "App install script complete"
