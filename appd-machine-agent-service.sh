fed7eea9a9131dfca53b13be33494f72f0b4cd02
vtlevin <troylevin@gmail.com>
2019-04-11 12:56:12 -0400

    Update appd-machine-agent-service.sh

--- appd-machine-agent-service.sh
+++ appd-machine-agent-service.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -x
 
-sudo curl -o /opt/appdynamics/machine-agent/conf/controller-info.xml https://raw.githubusercontent.com/vtlevin/cloudcenter/master/controller-info.xml
+sudo curl -o /opt/appdynamics/machine-agent/conf/controller-info.xml https://raw.githubusercontent.com/vtlevin/cloudcenter/master/controller-info-xml
 
-
-sudo curl -o /etc/sysconfig/appdynamics-machine-agent https://raw.githubusercontent.com/vtlevin/cloudcenter/master/appdynamics-machine-agent 
 sudo rpm -ivh /opt/appd/machine-agent/appdynamics-machine-agent-4.5.9.2096.x86_64.rpm
 
 
