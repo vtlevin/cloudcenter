0f228635a9d2e5050358efe4f883230f8ddc1e96
vtlevin <troylevin@gmail.com>
2019-04-11 07:39:23 -0400

    Update appd-mysql-agent-service.sh

--- appd-mysql-agent-service.sh
+++ appd-mysql-agent-service.sh
@@ -2,7 +2,6 @@
 
 agentSendLogMessage "Installing Appd MYSQL-Agent."
 
-#sudo java -jar /opt/appd/mysql-agent/db-agent.jar
-sudo nohup /opt/appd/mysql-agent/jre/bin/.NET - Dbagent.name="appd-mysql-agent" -jar /opt/appd/mysql-agent/db-agent.jar &
+sudo java -jar /opt/appd/mysql-agent/db-agent.jar
 
 agentSendLogMessage "App install script complete"
