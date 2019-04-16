5f65900106ed3ae1815ced51cab9118c723be103
vtlevin <troylevin@gmail.com>
2019-04-16 07:54:43 -0400

    Update appd-machine-agent.sh

--- appd-machine-agent.sh
+++ appd-machine-agent.sh
@@ -2,6 +2,8 @@
 
 sudo rpm -ivh /opt/appd/machine-agent/appdynamics-machine-agent-4.5.9.2096.x86_64.rpm
 
+systemctl stop appdynamics-machine-agent
+
 sudo curl -o /opt/appdynamics/machine-agent/conf/controller-info.xml https://raw.githubusercontent.com/vtlevin/cloudcenter/master/controller-info-xml
 
 systemctl restart appdynamics-machine-agent
