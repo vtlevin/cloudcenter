b5f35eadf379f3885689e3baf74339b59a52b920
vtlevin <troylevin@gmail.com>
2019-05-01 17:46:22 -0400

    Update appd-php-agent.sh

--- appd-php-agent.sh
+++ appd-php-agent.sh
@@ -10,7 +10,6 @@ chmod -R 755 /opt/appd/php-agent/logs
 echo "198.169.195.125 dangerous2019031501233911.saas.appdynamics.com" >> /etc/hosts
 
 sudo /opt/appd/php-agent/runme.sh
-#sudo $APPD_CONF_ACCOUNT_NAME $APPD_CONF_ACCESS_KEY $APPD_CONF_CONTROLLER_HOST $APPD_CONF_CONTROLLER_PORT $APPD_CONF_SSL_ENABLED $APPD_CONF_APP $APPD_CONF_TIER $APPD_PHP_PATH rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
 
 sudo systemctl stop httpd
 sudo systemctl start httpd
