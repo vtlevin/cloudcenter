f2ddb36ecdcb275777493cf6d5d01698f2bc1643
vtlevin <troylevin@gmail.com>
2019-05-01 10:16:40 -0500

    Update appd-php-agent.sh

--- appd-php-agent.sh
+++ appd-php-agent.sh
@@ -9,7 +9,8 @@ chmod -R 755 /opt/appd/php-agent/logs
 
 echo "198.169.195.125 dangerous2019031501233911.saas.appdynamics.com" >> /etc/hosts
 
-sudo /opt/appd/php-agent/runme.sh
+#sudo /opt/appd/php-agent/runme.sh
+sudo APPD_PHP_PATH=/opt/remi/php56/root/bin rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
 
 sudo systemctl stop httpd
 sudo systemctl start httpd
