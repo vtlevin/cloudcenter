14b10d55a5596dca5db92b5647e7acfd399dc19e
vtlevin <troylevin@gmail.com>
2019-05-01 11:51:24 -0500

    Update appd-php-agent.sh

--- appd-php-agent.sh
+++ appd-php-agent.sh
@@ -9,9 +9,8 @@ chmod -R 755 /opt/appd/php-agent/logs
 
 echo "198.169.195.125 dangerous2019031501233911.saas.appdynamics.com" >> /etc/hosts
 
-#sudo /opt/appd/php-agent/runme.sh
-sudo APPD_CONF_ACCOUNT_NAME='dangerous2019031501233911' APPD_CONF_ACCESS_KEY='xhibem365na4 APPD_CONF_CONTROLLER_HOST='dangerous2019031501233911.saas.appdynamics.com'
-APPD_CONF_CONTROLLER_PORT='443' APPD_CONF_SSL_ENABLED='true' APPD_CONF_APP='SIWAPP-HYBRID' APPD_CONF_TIER='App-Tier' APPD_PHP_PATH=/opt/remi/php56/root/bin rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
+sudo /opt/appd/php-agent/runme.sh
+#sudo $APPD_CONF_ACCOUNT_NAME $APPD_CONF_ACCESS_KEY $APPD_CONF_CONTROLLER_HOST $APPD_CONF_CONTROLLER_PORT $APPD_CONF_SSL_ENABLED $APPD_CONF_APP $APPD_CONF_TIER $APPD_PHP_PATH rpm -i appdynamics-php-agent-4.5.0.0-1.x86_64.rpm
 
 sudo systemctl stop httpd
 sudo systemctl start httpd
