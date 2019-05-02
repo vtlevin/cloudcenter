b1d66ebab7c19e17ebdb60409ed6d2a5995385fa
vtlevin <troylevin@gmail.com>
2019-05-02 17:30:16 -0400

    Update appd-machine-agent.sh

--- appd-machine-agent.sh
+++ appd-machine-agent.sh
@@ -11,8 +11,8 @@ systemctl stop appdynamics-machine-agent
 
 sudo curl -o /opt/appdynamics/machine-agent/conf/controller-info.xml https://raw.githubusercontent.com/vtlevin/cloudcenter/master/controller-info-xml
 
-for host in $cliqrNodeHostname; do
-    sudo sed -i '/\<tier-name\>App-Tier\<\/tier-name\>/a \<node-name\>'${host}'\<\/node-name\>' /opt/appdynamics/machine-agent/conf/controller-info.xml
-done   
+#for host in $cliqrNodeHostname; do
+#    sudo sed -i '/\<tier-name\>App-Tier\<\/tier-name\>/a \<node-name\>'${host}'\<\/node-name\>' /opt/appdynamics/machine-agent/conf/controller-info.xml
+#done   
   
 systemctl restart appdynamics-machine-agent
