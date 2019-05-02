414a1478cf1244572811b013ab5d05b0f83e641a
vtlevin <troylevin@gmail.com>
2019-05-01 22:43:54 -0500

    Update appd-php-machine-agent.sh

--- appd-php-machine-agent.sh
+++ appd-php-machine-agent.sh
@@ -11,6 +11,8 @@ systemctl stop appdynamics-machine-agent
 
 sudo curl -o /opt/appdynamics/machine-agent/conf/controller-info-php.xml https://raw.githubusercontent.com/vtlevin/cloudcenter/master/controller-info-xml
 
+sudo mv /opt/appdynamics/machine-agent/conf/controller-info-php.xml /opt/appdynamics/machine-agent/conf/controller-info.xml
+
 for host in $cliqrNodeHostname; do
     sudo sed -i '/\<tier-name\>App-Tier\<\/tier-name\>/a \<node-name\>'${host}'\<\/node-name\>' /opt/appdynamics/machine-agent/conf/controller-info.xml
 done   
